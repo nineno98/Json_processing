1e82acd9a70ad4026a27df712f3995d1d9c0aced
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-05-28 18:04:45 +0200

    Add project files.

---
+++ json_process_from_jsonfile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace json_process_from_jsonfile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
