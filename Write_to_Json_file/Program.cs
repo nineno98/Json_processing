c82957dc0182b4266ee2e4f09b71359ab242807e
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-05-29 11:37:14 +0200

    write to json file basic

---
+++ Write_to_Json_file/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_to_Json_file
+{
+    class Felhasznalo
+    {
+        private int _id;
+        private string _name;
+        private string _email;
+
+        public Felhasznalo(int id, string name, string email)
+        {
+            _id = id;
+            _name = name;
+            _email = email;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
