6fab538ec6af21471beb0530b8344ed92c9cbe0b
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-05-30 16:25:14 +0200

    write json

--- Write_to_Json_file/Program.cs
+++ Write_to_Json_file/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft;
+using Newtonsoft.Json;
+
 
 namespace Write_to_Json_file
 {
@@ -14,9 +18,18 @@ namespace Write_to_Json_file
 
         public Felhasznalo(int id, string name, string email)
         {
-            _id = id;
-            _name = name;
-            _email = email;
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public string Email { get => _email; set => _email = value; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Email}";
         }
     }
     
@@ -24,7 +37,62 @@ namespace Write_to_Json_file
     {
         static void Main(string[] args)
         {
+            
+            List<Felhasznalo> felhasznalok = feltolt();
+            
+            string json = JsonConvert.SerializeObject(felhasznalok);
+            Console.WriteLine(json);
+            File.WriteAllText("result.json", json);
+            Console.ReadKey();
+        }
 
+        static private List<Felhasznalo> feltolt()
+        {
+            List<Felhasznalo> felhasznList = new List<Felhasznalo>();
+            int id = 1;
+            do
+            {
+                Console.WriteLine("Új felhasználó: 1 | Kilépés: 2");
+                string bevitel = Console.ReadLine();
+                if (bevitel.Equals("1"))
+                {                  
+                    felhasznList.Add(ujFelhasznalo(id));
+                    id++;
+                }
+                else
+                {
+                    break;
+                }
+                
+            } while (true);
+            return felhasznList;
+        }
+
+        static private Felhasznalo ujFelhasznalo(int id)
+        {
+            Console.WriteLine("Add meg a felhasználó nevét:");
+            string nev = beker();
+            Console.WriteLine("Add meg az emailcímet:");
+            string email = beker();
+
+            Felhasznalo felhasznalo = new Felhasznalo(id, nev, email);
+            return felhasznalo;
+        }
+        static private string beker()
+        {
+            string res = "";
+            do
+            {
+                res = Console.ReadLine();
+                if (res != "")
+                {
+                    return res;
+                }
+                else
+                {
+                    Console.WriteLine("A bevitel nem lehet üres string.");
+                }
+            } while (true);
         }
     }
 }
