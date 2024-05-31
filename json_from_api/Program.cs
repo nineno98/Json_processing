1077fdd3fb444b02cb170315bb4d25b4301fd4ec
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-05-31 20:41:32 +0200

    json get kérés működik

--- json_from_api/Program.cs
+++ json_from_api/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace json_from_api
 {
@@ -13,16 +14,45 @@ namespace json_from_api
     {
         static void Main(string[] args)
         {
-            firstConnectToApi();
+            //firstConnectToApi();
+
+            var json = getJsonString().Result;
+            Console.WriteLine(json);
+
             Console.ReadKey();
         }
 
+        private static async Task<string> getJsonString()
+        {
+            string resJson = "";
+
+            try
+            {
+                var url = "http://localhost:8000/";
+                var api = "source.json";
+
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response =  client.GetAsync(api).Result;
+                resJson = await response.Content.ReadAsStringAsync();
+                return resJson;
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return resJson;
+        }
         private static async void firstConnectToApi()
         {
             try
             {
                 HttpClient client = new HttpClient();
                 string url = "http://localhost:8000/";
+
 
                 var response = await client.GetAsync(url);
 
