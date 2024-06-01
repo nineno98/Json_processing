e072ce8f609d653a3d176f1ad461c4bcbfdf7c4e
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-06-01 12:01:50 +0200

    api kész

---
+++ get_and_put_methods_with_jsonApi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_and_put_methods_with_jsonApi
+{
+    // GET: https://api-generator.retool.com/JbW1bY/employers
+    // GET filter https://api-generator.retool.com/JbW1bY/employers?name=value
+    // GET by id: https://api-generator.retool.com/JbW1bY/employers/1
+    // GET paginate https://api-generator.retool.com/JbW1bY/employers?_page=2&_limit=10
+    // POST: Content-Type: application/json' -d '{"key":"value"},
+    // https://api-generator.retool.com/JbW1bY/employers
+    // PUT: https://api-generator.retool.com/JbW1bY/employers/1
+    // PATCH: https://api-generator.retool.com/JbW1bY/employers/1
+    // DELETE: https://api-generator.retool.com/JbW1bY/employers/1
+
+    /*
+     * 
+     * {
+        "id": 1,
+        "name": "Karalee Radleigh",
+        "salary": 926
+        }
+     * 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
