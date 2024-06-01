b359c844c0f7fb27098f8d5306efcd4dafc38370
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-06-01 16:00:27 +0200

    összed dolgozó betöltése kész

---
+++ get_and_put_methods_with_jsonApi/Employer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace get_and_put_methods_with_jsonApi
+{
+    internal class Employer
+    {
+        private int _id;
+        private string _name;
+        private int _salary;
+
+        public Employer(int id, string name, int salary)
+        {
+            _id = id;
+            _name = name;
+            _salary = salary;
+        }
+
+        public int Id { get => _id; set => _id = value; }
+        public string Name { get => _name; set => _name = value; }
+        public int Salary { get => _salary; set => _salary = value; }
+
+        public override string ToString()
+        {
+            return $"id: {Id}, name: {Name}, salary: {Salary}";
+        }
+    }
+}
--- get_and_put_methods_with_jsonApi/Program.cs
+++ get_and_put_methods_with_jsonApi/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace get_and_put_methods_with_jsonApi
 {
@@ -27,8 +32,57 @@ namespace get_and_put_methods_with_jsonApi
      */
     internal class Program
     {
+        static public List<Employer> Employers = new List<Employer>();
         static void Main(string[] args)
         {
+
+            LoadEmployers();
+            //TestConnection();
+
+            foreach (var item in Employers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static async void LoadEmployers()
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://api-generator.retool.com/JbW1bY/employers");
+
+                var response = client.GetAsync(client.BaseAddress).Result;
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+
+                ProcessJsonString(jsonResponse);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void ProcessJsonString(string jsonstring)
+        {
+            var jsonArray = JArray.Parse(jsonstring);
+            foreach (var item in jsonArray)
+            {
+                Employer emp = JsonConvert.DeserializeObject<Employer>(item.ToString());
+                Employers.Add(emp);
+            }
+        }
+
+        public static async void TestConnection()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://api-generator.retool.com/JbW1bY/employers");
+
+            var response = await client.GetAsync(client.BaseAddress);
+            Console.WriteLine(response.StatusCode);
         }
     }
 }
