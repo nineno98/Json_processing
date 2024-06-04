75701cdf9ef72b153af274620fba4010670ef4fe
Marcell Kovács <marcell.kovacs123@gmail.com>
2024-06-04 20:48:05 +0200

    külalak

--- Employers_from_rest_api/Form1.Designer.cs
+++ Employers_from_rest_api/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.newEmployer = new System.Windows.Forms.Button();
+            this.modifiEmployer = new System.Windows.Forms.Button();
+            this.deleteEmployer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(415, 405);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // newEmployer
+            // 
+            this.newEmployer.Location = new System.Drawing.Point(514, 51);
+            this.newEmployer.Name = "newEmployer";
+            this.newEmployer.Size = new System.Drawing.Size(119, 39);
+            this.newEmployer.TabIndex = 1;
+            this.newEmployer.Text = "Új dolgozó...";
+            this.newEmployer.UseVisualStyleBackColor = true;
+            // 
+            // modifiEmployer
+            // 
+            this.modifiEmployer.Location = new System.Drawing.Point(514, 114);
+            this.modifiEmployer.Name = "modifiEmployer";
+            this.modifiEmployer.Size = new System.Drawing.Size(118, 39);
+            this.modifiEmployer.TabIndex = 2;
+            this.modifiEmployer.Text = "Módosítás...";
+            this.modifiEmployer.UseVisualStyleBackColor = true;
+            // 
+            // deleteEmployer
+            // 
+            this.deleteEmployer.Location = new System.Drawing.Point(515, 178);
+            this.deleteEmployer.Name = "deleteEmployer";
+            this.deleteEmployer.Size = new System.Drawing.Size(116, 41);
+            this.deleteEmployer.TabIndex = 3;
+            this.deleteEmployer.Text = "Dolgozó törlése";
+            this.deleteEmployer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.deleteEmployer);
+            this.Controls.Add(this.modifiEmployer);
+            this.Controls.Add(this.newEmployer);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button newEmployer;
+        private System.Windows.Forms.Button modifiEmployer;
+        private System.Windows.Forms.Button deleteEmployer;
     }
 }
 
